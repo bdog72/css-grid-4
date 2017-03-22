3bbef4e2755e32157b6d1cb0efb8967a6b26c727
Brian Bycynski <high5bri@hotmail.com>
2017-03-22 08:55:53 -0400

    new css grid examples

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div>
+      <h1>Resize the window</h1>
       <div className='wrap'>
         <div>1</div>
         <div>2</div>
@@ -16,6 +17,29 @@ class App extends Component {
       </div>
       <div className='wrap1'>
         <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </div>
+      <div className='wrap2'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+      <div className='wrap3'>
+        <div>1
+        <h1>Some</h1>
+        Content</div>
         <div>2</div>
         <div>3</div>
         <div>4</div>
